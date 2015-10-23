39f49ebc412206fd40a398c192e5d52f2795a38a
JuneYoungs <asplannedl@gmail.com>
2015-10-23 15:10:12 +0900

    void *

--- Header/main.c
+++ Header/main.c
@@ -1,8 +1,14 @@
 #include "L.h"
 
 void main() {
+	void *fp = calloc(1, sizeof(int));
+	int *fp2 = calloc(1, sizeof(int));
+	int k = 2;
+
+	fp = &k;
+	fp2 = fp;
+	fprintf(stderr, "%d\n", *fp2);
+	//FP("%d\n", *fp);
 	I(i);
-	display();
-	fprintf(stderr,"%d", i);
-	FP("%d", i);
+	FP("나는 %d입니다.\n", i);
 }
